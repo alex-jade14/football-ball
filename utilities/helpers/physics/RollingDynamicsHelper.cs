fcaeed64364dc0ee67f7dfa1bd669f7364b1f396
mint-user <alex_jade14@protonmail.com>
2025-02-02 14:13:15 +0100

    fix: add rotation reduction factor

--- utilities/helpers/physics/RollingDynamicsHelper.cs
+++ utilities/helpers/physics/RollingDynamicsHelper.cs
@@ -2,6 +2,8 @@ using Godot;
 using System;
 
 public partial class RollingDynamicsHelper{
+    public static float rotationReductionFactorBox = 4;
+    public static float interpolationCoefficient = 0.1f;
 
     public static (Vector3 linearVelocity, Vector3 angularVelocity) CalculateRollingWithoutSlipping(
         Vector3 force, Vector3 collisionPosition, float mass, Vector3 linearVelocity, Vector3 angularVelocity, bool canRoll){
